ad2c276858321ff583b4fa85c305a244389acbd8
terbit21org <106439510+terbit21org@users.noreply.github.com>
2024-07-22 05:13:27 +0700

    Add files via upload

---
+++ swap.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# size of swapfile in megabytes
+# chmod +x swap.sh
+# dos2unix swap.sh
+
+if [ ! "$#" -ge 1 ]; then
+    echo "Usage: $0 {size}"
+    echo "Example: $0 4G"
+    echo "(Default path: /swapfile)"
+    echo "Optional path: Usage: $0 {size} {path}"
+    exit 1
+fi
+
+swapsize=$1
+
+# does the swap file already exist?
+grep -q "swapfile" /etc/fstab
+
+# if not then create it
+if [ $? -ne 0 ]; then
+	echo 'swapfile not found. Adding swapfile.'
+	fallocate -l ${swapsize} /swapfile
+	chmod 600 /swapfile
+	mkswap /swapfile
+	swapon /swapfile
+	echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+else
+	echo 'swapfile found. No changes made.'
+  swapoff -a 
+	fallocate -l ${swapsize} /swapfile
+	chmod 600 /swapfile
+	mkswap /swapfile
+	swapon /swapfile
+	echo '/swapfile none swap defaults 0 0' >> /etc/fstab 
+ 
+fi
+
+# output results to terminal
+cat /proc/swaps
+cat /proc/meminfo | grep Swap
